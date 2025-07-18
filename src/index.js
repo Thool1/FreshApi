dc8a86ba9b39db93da70666cb3fca69d0d1915f2
ThooL <thools1e2@gmail.com>
2025-07-18 13:44:57 +0530

    i dont know  what to to add

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ app.post('/articles', express.json(), (req, res) => {
     // const title = req.body.title
     const { title, slug, content, excerpt, author, category, tags, image, publishedAt, isFeatured } = req.body
     const { name, bio, profilePicture } = author;
-    // articles.push({id:articles.length + 1,title,slug,content,excerpt,author})
     const newArticle = {
         id: articles.length + 1,
         title,
@@ -47,7 +46,33 @@ app.post('/articles', express.json(), (req, res) => {
     }
     articles.push(newArticle);
     res.json({ data: newArticle })
+})
 
+app.put('/articles/:id', (req, res) => {
+    const articleId = parseInt(req.params.id, 10)
+    const articleIndex = articles.findIndex(a => a.id === articleId)
+    if (articleIndex !== -1) {
+        articles[articleIndex].title = req.body.title
+        res.json(articles[articleIndex])
+    } else {
+        res.status(400).json({ error: "Article not found" })
+    }
+});
+app.delete('/articles/:id', (req, res) => {
+    const articleId = parseInt(req.params.id, 10)
+    const articleIndex = articles.findIndex(a => a.id === articleId)
+    if (articleIndex !== -1) {
+
+        articles.splice(articleIndex, 1)
+        res.status(204).json({ data: { id: articleId } })
+        // res.json(articles[articleIndex])
+    } else {
+        res.status(400).json({ error: "Article not found" })
+    }
+});
+
+app.use((req, res) => {
+    res.status(404).json({ Error: 'Nothing Found' })
 })
 
 app.listen(PORT, () => {
