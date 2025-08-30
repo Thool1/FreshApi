535577c4b9ae3033e0fdde30cdea8d4e18e1159d
saurabh <112417184+Thool1@users.noreply.github.com>
2025-08-30 19:11:03 +0530

    Update Article.js

--- src/models/Article.js
+++ src/models/Article.js
@@ -75,7 +75,7 @@ const articleSchema = new mongoose.Schema({
   //   }
   // },
 bollywood: {
-  isBollywood : false,
+  isBollywood: { type: Boolean, default: false },
   title: { type: String, default: null },
   language: { type: String, default: null },
   releaseDate: { type: String, default: null },
